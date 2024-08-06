c4e034602305b54417ec501e53e698763b8201eb
HusslinHq <support@husslin.com>
2024-08-06 20:54:54 +0100

    fixes for FAQ Page

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -5,33 +5,30 @@ import SeventhSection from "./SeventhSection"
 import FAQAccordion from "./FAQAccordion"
 function FAQ() {
   return (
-    <div>
-         <>
-    <div className='first-sectionBg  lg:text-center'>
+    <div className="text-center">
+     <div className='first-sectionBg  lg:text-center'>
     <div className="first-section mt-[10px] ">
-    <div className="text-[#007BFF] text-[20px] capitalize font-bold font-Euclid text-4xl leading-[32px] lg:text-[70px] lg:leading-[85px]">
+    <div className="text-[#007BFF] text-[20px] capitalize font-bold font-Euclid leading-[18px] lg:text-[70px] lg:leading-[85px]">
     Frequently Asked Questions
     </div>
-    <div className=" p-4 text-[10px] font-Euclid lg:text-[30px] lg:leading-normal font-normal capitalize">
+    <div className="p-2 text-[10px] font-Euclid lg:text-[30px] lg:leading-normal font-normal capitalize">
     Need any Answer? Enter your question for quick response
     </div>
     </div>
     </div>
-   <div>
+   <div className="p-8">
     <FAQAccordion/>
    </div>
-       
-    <div>
-        <ValuesModel/>
-      </div>
+       <div className="bg-[#007BFF] p-2 m-6 rounded-md lg:mb-[174px] lg:mt-[131px]">
+        <p className="text-[11.5px] font-light text-[#FFF] lg:text-[40px] lg:leading-[113px] lg:font-medium">Not Satisfied? Send a mail to support@husslin.com</p>
+       </div>
     <div className="p-4">
         <SixthSection/>
     </div>
     <div>
         <SeventhSection/>
     </div>
-    </>
-    </div>
+     </div>
   )
 }
 
--- src/pages/FAQAccordion.tsx
+++ src/pages/FAQAccordion.tsx
@@ -8,36 +8,41 @@ const FAQAccordion = () => {
     };
 
     return (
-        <div className="accordion">
+        <div className="accordion ">
             <div className="accordion-item">
-                <div className="accordion-title bg-[#E1F0FF] font-bold" onClick={() => toggleAccordion(0)}>
-                    <h3 className=''>What is Husslin?</h3>
+                <div className="accordion-title bg-[#E1F0FF] font-bold lg:text-[30px] lg:gap-[52px]" onClick={() => toggleAccordion(0)}>
                     <span>{activeIndex === 0 ? '-' : '+'}</span>
+                    <h3 className=''>What is Husslin?</h3>
+
                 </div>
                 {activeIndex === 0 && (
-                    <div className="accordion-content ">
-                        <p>Husslin is an app that helps you get things done by connecting you with verified artisans for various services.</p>
+                    <div className="accordion-content lg:text-[25px] lg:p-6 p-4">
+                        <p>You can sign up for Husslin by downloading the app 
+                            from the App Store or Google Play Store. Once you've 
+                            downloaded the app, you can sign up using your phone 
+                            number or email address. After signing up, you can start 
+                            using Husslin to get things done..</p>
                     </div>
                 )}
             </div>
             <div className="accordion-item ">
-                <div className="accordion-title font-bold" onClick={() => toggleAccordion(1)}>
+                <div className="accordion-title font-bold  lg:text-[30px] lg:gap-[52px]" onClick={() => toggleAccordion(1)}>
+                <span>{activeIndex === 1 ? '-' : '+'}</span>
                     <h3>How do I sign up?</h3>
-                    <span>{activeIndex === 1 ? '-' : '+'}</span>
                 </div>
                 {activeIndex === 1 && (
-                    <div className="accordion-content">
+                    <div className="accordion-content lg:text-[25px] lg:p-6 p-4">
                         <p>You can sign up for Husslin by downloading the app from the App Store or Google Play Store. Once you&apos;ve downloaded the app, you can sign up using your phone number or email address. After signing up, you can start using Husslin to get things done.</p>
                     </div>
                 )}
             </div>
             <div className="accordion-item ">
-                <div className="accordion-title font-bold" onClick={() => toggleAccordion(2)}>
+                <div className="accordion-title font-bold  lg:text-[30px] lg:gap-[52px]" onClick={() => toggleAccordion(2)}>
+                <span>{activeIndex === 2 ? '-' : '+'}</span>
                     <h3>How do I use Husslin?</h3>
-                    <span>{activeIndex === 2 ? '-' : '+'}</span>
                 </div>
                 {activeIndex === 2 && (
-                    <div className="accordion-content">
+                    <div className="accordion-content lg:text-[25px] lg:p-6 p-4 ">
                         <p>You can use Husslin by...</p> {/* Add the relevant content */}
                     </div>
                 )}
